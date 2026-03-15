2f7c680dba6e42d8ac293cd46d30b15381d25069
Ishant Sharma <ishant474815@gmail.com>
2026-03-15 17:05:31 +0000

    QuizApp

--- src/main/java/com/Qauser/Quaser/Controller/QuesController.java
+++ src/main/java/com/Qauser/Quaser/Controller/QuesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ques)
+@RequestMapping("/ques")
 public class QuesController {
 
     private final QuestionService questionService;
