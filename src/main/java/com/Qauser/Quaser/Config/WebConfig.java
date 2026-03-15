ece0b660faac6e2eec4aabb6cec6aaee0ca6e316
Ishant Sharma <ishant474815@gmail.com>
2026-03-15 17:06:11 +0000

    QuizApp

---
+++ src/main/java/com/Qauser/Quaser/Config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.Qauser.Quaser.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
